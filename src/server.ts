29260a70b2cb9a764ebc6942452eec1b1c7b3eb4
Felipe Rodrigues Batista <feliperodriguesbatista@outlook.com>
2026-02-06 13:41:13 -0300

    fix port

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("🚀 API rodando na porta 3000");
 });
