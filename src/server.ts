64cf59342d7ce8f7cc90ec2b25c20bd11c77d0b3
Felipe Rodrigues Batista <feliperodriguesbatista@outlook.com>
2026-04-22 15:15:39 -0300

    feat: add cors policy

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import "dotenv/config";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json({ type: "*/*" }));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: "https://payables-wizard.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true, // only if you use cookies/auth
+}));
+
+app.use(express.json());
+
 
 app.use(routes);
 
